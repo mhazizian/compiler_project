fc198cbe13aeed37bb5698a3f686d09a36189213
Mohammad Hosein Azizian <mohammadh.azizian@gmail.com>
2018-11-20 10:38:11 +0330

    amend run_antler script

--- run_antler.sh
+++ run_antler.sh
@@ -1,11 +1,15 @@
+# example to run script:
+# ./run_antler.sh smoolaParser program
+
 export CLASSPATH=".:/usr/local/lib/antlr-4.7.1-complete.jar:$CLASSPATH"
-rm *.class
-rm *.tokens
-rm $1*.java
+
+./clean.sh $1
 
 # antler4 test.g4
-java -jar /usr/local/lib/antlr-4.7.1-complete.jar $1.g4
-
+java -jar /usr/local/lib/antlr-4.7.1-complete.jar $1Lexer.g4
+java -jar /usr/local/lib/antlr-4.7.1-complete.jar $1Parser.g4
 
 javac *.java
 java org.antlr.v4.gui.TestRig $1 $2 -gui < in.sml
+
+./clean.sh $1
