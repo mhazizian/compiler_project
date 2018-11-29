657604d1e741bc7bc5b7bd8c2ca760c662e9a74b
Mohammad Hosein Azizian <mohammadh.azizian@gmail.com>
2018-11-29 13:40:26 +0330

    Amend SymbolTableClassItem class

--- symbolTable/SymbolTableClassItem.java
+++ symbolTable/SymbolTableClassItem.java
@@ -1,18 +1,49 @@
 package symbolTable;
 
 import ast.Type.Type;
-
-import java.util.ArrayList;
+import java.util.*;
 
 public class SymbolTableClassItem extends SymbolTableItem {
 
-    // ArrayList<Type> argTypes = new ArrayList<>();
-    // SymbolTableClassItem parent = new SymbolTableClassItem()
+    SymbolTableClassItem parent;
+    public HashMap<String, SymbolTableItem> items;
 
     public SymbolTableClassItem(String name) {
         this.name = name;
-        // this.argTypes = argTypes;
+        this.items = new HashMap<String, SymbolTableItem>();
     }
+
+    public SymbolTableClassItem(String name, SymbolTableClassItem newParent) {
+        this.name = name;
+        this.parent = newParent;
+        this.items = new HashMap<String, SymbolTableItem>();
+    }
+
+    public void put(SymbolTableItem item) throws ItemAlreadyExistsException {
+        if(items.containsKey(item.getKey()))
+            throw new ItemAlreadyExistsException();
+        items.put(item.getKey(), item);
+    }
+
+    public SymbolTableItem get(String key) throws ItemNotFoundException {
+        SymbolTableItem value = items.get(key);
+        if(value == null && parent != null)
+            return parent.get(key);
+        else if(value == null)
+            throw new ItemNotFoundException();
+        else
+            return value;
+    }
+
+
+    public SymbolTableClassItem getPreSymbolTableClassItem() {
+        return parent;
+    }
+
+    public void setParent(SymbolTableClassItem newParent) {
+        this.parent = newParent;
+    }
+
 
     @Override
     public String getKey() {
