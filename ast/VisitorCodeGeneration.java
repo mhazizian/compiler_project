9416a4d839640d973e2009627e86b4189f4cb283
Mohammad Hosein Azizian <mohammadh.azizian@gmail.com>
2018-12-24 17:12:47 +0330

    Remove extra parts from VisitorCodeGeneration

--- ast/VisitorCodeGeneration.java
+++ ast/VisitorCodeGeneration.java
@@ -25,8 +25,7 @@ import ast.Type.PrimitiveType.IntType;
 import ast.Type.UserDefinedType.UserDefinedType;
 import ast.Type.NoType.NoType;
 import symbolTable.*;
-public class VisitorImpl implements Visitor {
-    private static int ItemDecIndex = 0;
+public class VisitorCodeGeneration implements Visitor {
     public static String objectClassName = "Object";
     public static Type thisObjectType = new NoType();
 
@@ -279,163 +278,39 @@ public class VisitorImpl implements Visitor {
 
     @Override
     public void visit(Program program) {
-        createNewSymbolTable();
-        SymbolTableClassItem objectClass = new SymbolTableClassItem(objectClassName);
-        putToSymbolTable(objectClass);
-
         ArrayList<ClassDeclaration> classes =
             ((ArrayList<ClassDeclaration>)program.getClasses());
         ClassDeclaration mainClass = program.getMainClass();
-
-        if (classes.size() == 0 && mainClass == null) {
-            System.out.println("Line:" + 0 +
-                ":No class exists in the program");
-            SymbolTable.isValidAst = false;
-            return;
-        }
-
-        // create SymbolTableItem for each classDec
-        putToSymbolTable(this.createClassDecSymbolTableItem(mainClass));
-        completeClassSymbolTableBody(mainClass);
-
-        ArrayList<Boolean> classDecIsValid = new ArrayList<Boolean>();
-        for (int i = 0; i < classes.size(); i++) {
-            try {
-                SymbolTable.top.put(this.createClassDecSymbolTableItem(
-                    classes.get(i))
-                );
-                classDecIsValid.add(true);
-            } catch (ItemAlreadyExistsException error) {
-                System.out.println("Line:" + classes.get(i).getLineNumber() +
-                    ":Redefinition of class " +
-                    classes.get(i).getName().getName()
-                );
-                classDecIsValid.add(false);
-            }
-        }
-        // add parentClass for each ClassDecSymbolTableItem
-        for (int i = 0; i < classes.size(); i++) {
-            String parentClassName = classes.get(i).getParentName().getName();
-            if (parentClassName.equals(""))
-                parentClassName = objectClassName;
-
-            if (parentClassName.equals(classes.get(i).getName().getName())) {
-                System.out.println("Line:" + classes.get(i).getLineNumber() +
-                    ":self-inheritance is not valid, class name: " +
-                    parentClassName);
-                SymbolTable.isValidAst = false;
-                continue;
-            }
-
-            try {
-                SymbolTableClassItem parentClass = ((SymbolTableClassItem)
-                        SymbolTable.top.get("c_" + parentClassName));
-                SymbolTableClassItem curretClass = ((SymbolTableClassItem)
-                        SymbolTable.top.get("c_" + classes.get(i).getName().getName()));
-
-                if (!classDecIsValid.get(i))
-                        continue;
-
-                curretClass.setParent(parentClass);
-
-            } catch (ItemNotFoundException error) {
-                System.out.println("Line:" + classes.get(i).getLineNumber() +
-                    ":inherited class not found: " + parentClassName);
-                SymbolTable.isValidAst = false;
-            }
-        }
-
-        // Complete classSymbolTableItem body with proper order:
-        int i = 0;
-        HashMap<String, Boolean> visitedClasses = new HashMap<>(); 
-        visitedClasses.put(objectClassName, true);
-        visitedClasses.put("", true);
-
-        while(visitedClasses.size() < classes.size() + 2) {
-            if (visitedClasses.getOrDefault(classes.get(i).getName().getName(), false))
-                continue;
-            
-            if (visitedClasses.getOrDefault(classes.get(i).getParentName().getName(), false)
-                || !(SymbolTable.top.hasItem(classes.get(i).getParentName().getName()))
-            ) {
-                this.completeClassSymbolTableBody(classes.get(i));
-                visitedClasses.put(classes.get(i).getName().getName(), true);
-            }
-            i = (i + 1) % classes.size();
-        }
 
         for (int j = 0; j < classes.size(); j++)
             classes.get(j).accept(new VisitorImpl());
 
         mainClass.accept(new VisitorImpl());
-        SymbolTable.pop();
     }
 
     @Override
     public void visit(ClassDeclaration classDeclaration) {
-        createNewSymbolTable();
-        VisitorImpl.thisObjectType = new UserDefinedType(classDeclaration.getName());
+        ArrayList<VarDeclaration> vars =
+            ((ArrayList<VarDeclaration>)classDeclaration.getVarDeclarations());
+        ArrayList<MethodDeclaration> methods =
+            ((ArrayList<MethodDeclaration>)classDeclaration.getMethodDeclarations());
 
-        try {
-            SymbolTableClassItem currentClass =
-                ((SymbolTableClassItem) SymbolTable.top.get(
-                "c_" + classDeclaration.getName().getName()));
+        // visit subItems:
+        for (int i = 0; i < vars.size(); i++)
+            vars.get(i).accept(new VisitorImpl());
 
-            ArrayList<VarDeclaration> vars =
-                ((ArrayList<VarDeclaration>)classDeclaration.getVarDeclarations());
-            ArrayList<MethodDeclaration> methods =
-                ((ArrayList<MethodDeclaration>)classDeclaration.getMethodDeclarations());
-
-            // add subItems to SymbolTableItem and ClassSymbolTable:
-            currentClass.addSubItemsToSymbolTable();
-            // SymbolTable.top.printItems();
-
-
-            // visit subItems:
-            for (int i = 0; i < vars.size(); i++)
-                vars.get(i).accept(new VisitorImpl());
-
-            for (int i = 0; i < methods.size(); i++)
-                methods.get(i).accept(new VisitorImpl());
-
-        } catch (ItemNotFoundException error) {
-            // System.out.println("____ ItemNotFoundException.");
-        }
-        SymbolTable.pop();
+        for (int i = 0; i < methods.size(); i++)
+            methods.get(i).accept(new VisitorImpl());
     }
 
     @Override
     public void visit(MethodDeclaration methodDeclaration) {
-        createNewSymbolTable();
         Expression returnValue = methodDeclaration.getReturnValue();
         Identifier name = methodDeclaration.getName();
         ArrayList<VarDeclaration> args = methodDeclaration.getArgs();
         ArrayList<VarDeclaration> localVars =
             methodDeclaration.getLocalVars();
         ArrayList<Statement> body = methodDeclaration.getBody();
-
-        for (int i = 0; i < args.size(); i++) {
-            try {
-                SymbolTable.top.put(this.createVarDecSymbolItem(args.get(i)));
-            } catch (ItemAlreadyExistsException error) {
-               System.out.println("Line:" + args.get(i).getLineNumber()
-                  + ":Redefinition of variable " +
-                  args.get(i).getIdentifier().getName());
-              SymbolTable.isValidAst = false;
-            }
-        }
-
-        for (int i = 0; i < localVars.size(); i++) {
-            try {
-                SymbolTable.top.put(this.createVarDecSymbolItem(localVars.get(i)));
-            } catch (ItemAlreadyExistsException error) {
-               System.out.println("Line:" + localVars.get(i).getLineNumber()
-                  + ":Redefinition of variable " +
-                  localVars.get(i).getIdentifier().getName());
-              SymbolTable.isValidAst = false;
-            }
-        }
-
 
         name.accept(new VisitorImpl());
 
@@ -450,35 +325,12 @@ public class VisitorImpl implements Visitor {
             body.get(i).accept(new VisitorImpl());
 
         returnValue.accept(new VisitorImpl());
-
-        if (!canAssign(methodDeclaration.getReturnType(), returnValue.getType()))
-        {
-            System.out.println("Line:" + returnValue.getLineNumber() + ":"
-                + methodDeclaration.getName().getName() + " return type must be "
-                + methodDeclaration.getReturnType()
-            );
-            SymbolTable.isValidAst = false;
-        }
-
-        SymbolTable.pop();
     }
 
     @Override
     public void visit(VarDeclaration varDeclaration) {
         Identifier identifier = varDeclaration.getIdentifier();
         identifier.accept(new VisitorImpl());
-
-        if (varDeclaration.getType().getType() == TypeName.userDefinedType) {
-            String className = ((UserDefinedType)varDeclaration.getType()).getName().getName();
-            try {
-                SymbolTableItem item = SymbolTable.top.get("c_" + className);
-            } catch (ItemNotFoundException e) {
-                System.out.println("Line:" + varDeclaration.getLineNumber() + ":class "
-                    + className + " is not declared"
-                );
-                SymbolTable.isValidAst = false;
-            }
-        }
     }
 
      @Override
@@ -490,15 +342,6 @@ public class VisitorImpl implements Visitor {
 
         instance.accept(new VisitorImpl());
         index.accept(new VisitorImpl());
-
-        // only array of int is valid:
-        arrayCall.setType(new IntType());
-
-        if (index.getType().getType() != TypeName.intType &&
-                index.getType().getType() != TypeName.noType) {
-            System.out.println("Line:" + arrayCall.getLineNumber() + ":array index must be int");
-            SymbolTable.isValidAst = false;
-        }
     }
 
     @Override
@@ -508,70 +351,20 @@ public class VisitorImpl implements Visitor {
 
         left.accept(new VisitorImpl());
         right.accept(new VisitorImpl());
-
-        String leftType = getType(left);
-        String rightType = getType(right);
-
-        BinaryOperator operator = binaryExpression.getBinaryOperator();
-
-        // Both the operands should be valid
-        if (isArithmetic(operator))
-            checkOperandsValidity(leftType, rightType, binaryExpression, "int");
-        else 
-            checkOperandsValidity(leftType, rightType, binaryExpression, "bool");
     }
 
     @Override
     public void visit(Identifier identifier) {
-        try {
-            SymbolTableItem item = SymbolTable.top.getItem(identifier.getName());
-
-            if (item.getItemType() == SymbolTableItemType.variableType) 
-                identifier.setType(((SymbolTableVariableItem)item).getType());
-            
-            if (item.getItemType() == SymbolTableItemType.NoType) 
-                identifier.setType(new NoType());
-                
-        } catch (ItemNotFoundException error) {
-            if (!identifier.getName().equals("")) {
-                System.out.println("Line:" + identifier.getLineNumber() + ":variable "
-                    + identifier.getName() + " is not declared"
-                );
-                SymbolTable.isValidAst = false;
-                try {
-                    SymbolTable.top.put(new SymbolTableNoTypeItem(identifier.getName()));
-                } catch(ItemAlreadyExistsException e) {}
-                identifier.setType(new NoType());
-            }
-        }
     }
 
     @Override
     public void visit(MethodCallIdentifier identifier) {
-        try {
-            SymbolTableItem item = SymbolTable.top.getItem(identifier.getName());
-
-            if (item.getItemType() == SymbolTableItemType.variableType) 
-                identifier.setType(new UserDefinedType(new Identifier(item.getName())));
-            
-        } catch (ItemNotFoundException error) {
-            // :thinking:
-        }
     }
 
     @Override
     public void visit(Length length) {
         Expression expression = length.getExpression();
         expression.accept(new VisitorImpl());
-
-        String type = getType(expression);
-        if (isValidType(type, "int[]"))
-            length.setType(new IntType());
-        else {
-            // This error message doesn't exist in project description
-            System.out.println("Line:" + length.getLineNumber() + ": unsupported type for length");
-            length.setType(new NoType());
-        }
     }
 
     @Override
@@ -584,30 +377,6 @@ public class VisitorImpl implements Visitor {
         methodName.accept(new VisitorImpl());
         for (int i = 0; i < args.size(); i++)
             args.get(i).accept(new VisitorImpl());
-
-        if (methodCall.getLineNumber() == -1)
-            methodCall.setLineNumber(instance.getLineNumber());
-
-
-        try {
-            SymbolTableItem instanceItem = SymbolTable.top.getItem(instance.getType().toString());
-            
-            if (instanceItem.getItemType() == SymbolTableItemType.classType)
-                findTheMethodInClass(methodCall, instanceItem,
-                        methodName.getName(), instance.getType().toString());
-
-            else if (instanceItem.getItemType() == SymbolTableItemType.variableType) {
-                Type varType = ((SymbolTableVariableItem)instanceItem).getType();
-
-                if (varType.getType() == TypeName.userDefinedType)
-                    findTheClass(methodCall, methodName.getName(), varType.toString());
-                else {
-                }
-            }
-        } catch (ItemNotFoundException error) {
-            System.out.println("Line:" + methodName.getLineNumber() + ":primitive types are not callable.");
-            SymbolTable.isValidAst = false;
-        }
     }
 
     @Override
@@ -622,13 +391,10 @@ public class VisitorImpl implements Visitor {
     public void visit(NewClass newClass) {
         Identifier className = newClass.getClassName();
         className.accept(new VisitorImpl());
-        // className is an Identifier not a primitive type (It's been checked in parser)
-        // newClass.setType(new UserDefinedType(className));
     }
 
     @Override
     public void visit(This instance) {
-        // instance.setType(VisitorImpl.thisObjectType);
     }
 
     @Override
@@ -659,15 +425,10 @@ public class VisitorImpl implements Visitor {
 
     @Override
     public void visit(Block block) {
-        // Nothing to type check in 'block'
-        createNewSymbolTable();
-
         ArrayList<Statement> body = block.getBody();
 
         for (int i = 0; i < body.size(); ++i)
             body.get(i).accept(new VisitorImpl());
-
-        SymbolTable.pop();
     }
 
     @Override
@@ -677,15 +438,10 @@ public class VisitorImpl implements Visitor {
         Statement alternativeBody = conditional.getAlternativeBody();
 
         expression.accept(new VisitorImpl());
-        
-        createNewSymbolTable();
         consequenceBody.accept(new VisitorImpl());
-        SymbolTable.pop();
 
         if (alternativeBody != null) {
-            createNewSymbolTable();
             alternativeBody.accept(new VisitorImpl());
-            SymbolTable.pop();
         }
 
     }
